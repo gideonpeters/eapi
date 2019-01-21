a4e56ffd7dcb8fafb25ce7b2113091a0e10a7923
Gideon Peters <gideonpeters85@gmail.com>
2019-01-21 14:09:30 +0100

    updated database

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -1,9 +1,15 @@
 <?php
 
 use Faker\Generator as Faker;
+use App\Model\Product;
 
-$factory->define(App\Model\Product::class, function (Faker $faker) {
+$factory->define(Product::class, function (Faker $faker) {
     return [
         //
+        'name' => $faker->word,
+        'details' => $faker->paragraph,
+        'price' => $faker->numberBetween(100, 1000),
+        'stock' => $faker->randomDigit,
+        'discount' => $faker->numberBetween(2,30)
     ];
 });
